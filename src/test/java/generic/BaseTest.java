e595264563906cc73a592f213daf0784f6eafe25
itl <itl@no>
2023-11-12 15:09:31 +0530

    selenium framework

---
+++ src/test/java/generic/BaseTest.java
@@ -0,0 +1,28 @@
+package generic;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+	public WebDriver driver;
+	
+	@BeforeMethod
+	public void preCondition()
+	{
+		driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	@AfterMethod
+	public void postCondition()
+	{
+		driver.quit();
+	}
+
+}
